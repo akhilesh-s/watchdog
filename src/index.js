9f3bd7c33672b673b1212accd43d82200ea54852
akhilesh <aksi9665@gmail.com>
2024-08-26 23:06:37 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import { Octokit } from "octokit";
+import dayjs from "dayjs";
+import axios from "axios";
+import * as core from '@actions/core';
+
+(() => {
+  try {
+    console.log("Initializing Script...");
+
+    const owner = core.getInput("OWNER") || "";
+    const repo = core.getInput("REPO") || "";
+    const slackWebhookURL = core.getInput("SLACK_WEBHOOK_URL") || "";
+    const token = core.getInput("GITHUB_TOKEN") || "";
+
+    // Check if required values are provided
+    if (!token || !owner || !repo || !slackWebhookURL) {
+      throw new Error(
+        "Required inputs (GITHUB_TOKEN, OWNER, REPO, SLACK_WEBHOOK_URL) are not provided."
+      );
+    }
+
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    (async () => {
+      const { data: pullRequests } = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        state: "open",
+      });
+
+      const oldPRs = pullRequests.filter((pr) => {
+        const createdAt = dayjs(pr.created_at);
+        return dayjs().diff(createdAt, "day") > 2;
+      });
+
+      if (oldPRs.length > 0) {
+        console.log(`Found ${oldPRs.length} PRs open for more than 2 days.`);
+        let messageArr = [];
+        oldPRs.forEach((pr) => {
+          const text = `- PR #${pr.number}: ${pr.title} (created on ${dayjs(
+            pr.created_at
+          )}) by ${pr.user.login}`;
+          messageArr.push(text);
+        });
+
+        const slackMessage = messageArr.join("\n");
+        console.log("Slack message to be sent:", slackMessage);
+
+        // Send message to Slack
+        await axios.post(slackWebhookURL, {
+          text: slackMessage,
+        });
+        console.log("Slack notification sent.");
+      } else {
+        const text = "No PRs open for more than 2 days.";
+        console.log(text);
+        await axios.post(slackWebhookURL, {
+          text: slackMessage,
+        });
+      }
+    })();
+  } catch (err) {
+    console.log("err", err);
+  }
+})();
